a665647ce5c1140f7abf1683855812dd6b1badd7
Ngan Nguyen <saoaz1029@gmail.com>
2025-11-27 22:30:08 +0700

    Config FE for AWS Edge Services: SPA mode, Amplify build, WAF error handling

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from '@react-router/dev/config'
 
 export default {
-  // Config options...
+    // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
   // nếu là true thì sẽ render phía server, false thì là SPA
   //server thì sẽ cần môi trường nodejs để chạy server render, ko cần môi trường nodejs nếu SPA
   //server ko thể deploy lên github page đc vì github page chỉ hỗ trợ SPA

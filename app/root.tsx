65503851fcb2e4f6ec1a3bc0104ad66fbffaf532
Chill <baophi852@gmail.com>
2025-12-11 22:21:34 +0700

    fix UI

--- app/root.tsx
+++ app/root.tsx
@@ -25,7 +25,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' type='image/png' href='/Logo_AWS_FCJ.png' />
-        <title>StudentManager Pro - LMS</title>
+        <title>Serverless Student Management</title>
         <script src='https://cdn.tailwindcss.com'></script>
         <script src='https://accounts.google.com/gsi/client' async defer></script>
         <link
